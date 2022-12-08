72fc01d6e9165a7c76c14ebba86edd4725153ef0
Den <workplacel@mail.ru>
2022-12-08 07:23:11 +0500

    Lesson5 in progress

--- main.py
+++ main.py
@@ -1,113 +1,126 @@
-from math import pi, fabs
-import random
+import random as r
 
-'''Задача 1. Вычислить число c заданной точностью d'''
+'''1 Напишите программу, удаляющую из файла все слова, содержащие "абв".'''
 
-# d = float(input("Введите точность"))
-# i, m, pi_ = 1, 1, 0
+# file = open('text.txt', 'r', encoding='utf-8')
+# result = file.read().split(' ')
+# file.close()
+# result1 = ''
+# for i in result:
+#     if 'абв' in i: continue
+#     result1 += i + ' '
+# print(result1)
+
+
+'''2 Создайте программу для игры с конфетами человек против человека.
+
+Условие задачи: На столе лежит 2021 конфета.
+Играют два игрока делая ход друг после друга.
+Первый ход определяется жеребьёвкой.
+За один ход можно забрать не более чем 28 конфет.
+Все конфеты оппонента достаются сделавшему последний ход.
+Сколько конфет нужно взять первому игроку, чтобы забрать все
+конфеты у своего конкурента?
+
+a) Добавьте игру против бота
+b) Подумайте как наделить бота "интеллектом"'''
+
+# last = 121
+# player_num = 1
+# while last > 0:
+#     if player_num > 0:
+#         player = 'player 1'
+#     else:
+#         player = 'player 2'
+#     if last <= 28:
+#         num = int(input(f"{player} Input number from 0 to {last} \n"))
+#     else:
+#         num = int(input(f"{player} Input number from 0 to 28 \n"))
+#     if num <= 28 and num <= last:
+#         last -= num
+#     else: print(f"Hey! {player}! Your number is out of range")
+#     print(f'{last} is remain \n')
+#     player_num *= -1
+#     if last == 0: print(f"{player} You win")
+
+'''a) Добавьте игру против бота'''
+
+
+# def func(last):
+#     if last <= 28:
+#         num = r.randint(0, last)
+#     else:
+#         num = r.randint(0, 28)
+#     print(f'The bot took {num}')
+#     if last - num == 0:
+#         print("The bot win")
+#         return 0
+#     else: return last - num
 #
-# while abs(pi_ - pi) >= d:
-#     pi_ += 4/(i * 2 - 1) * m
-#     i += 1
-#     m *= -1
-# print(pi_, pi, sep='\n')
-
-'''Задача 1. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N'''
-
-# N = int(30030)
-# res = []
 #
-# while N != 1:
-#     if N % 2 == 0:
-#         res.append(2)
-#         N /= 2
-#     if N % 2 != 0:
-#         for i in range(3, int(N+1), 2):
-#             if N % i == 0:
-#                 res.append(i)
-#                 N /= i
-#                 break
-# print(res)
-
-'''Задача 2. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.'''
-
-# lst = [1, 1, 2, 2, 3, 4, 4, 5, 'dog', 6, 6, 7] # [3, 5, 7]
-# dct = dict()
+# last = 80
 #
-# for i in lst:
-#     if i not in dct:
-#         dct[i] = 0
-#     dct[i] += 1
+# while last > 0:
+#     if 0 < last <= 28:
+#         num = int(input(f"Input number from 0 to {last} \n"))
+#     else:
+#         num = int(input(f"Input number from 0 to 28 \n"))
+#     if num <= 28 and num <= last:
+#         last -= num
+#     else: print(f"Your number is out of range")
+#     print(f'{last} is remain \n')
 #
-# print([x for x in dct if dct[x] == 1])
+#     if last == 0:
+#         print(f"You win")
+#         break
+#     last = func(last)
 
-'''Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) 
-многочлена и записать в файл многочлен степени k.'''
+
+'''3 Создайте программу для игры в "Крестики-нолики".
+
+Вариант интерфейса:
+
+ 1  |  2 | 3
+--------------
+ 4  |  5 | 6
+--------------
+ 7  |  8 | 9
+'''
 
 
-# def get_random_funck(k):
-#     lst = list()
-#     for i in range(k-1, -1, -1):
-#         if random.randint(0, 10) > 4:
-#             if random.randint(0, 10) % 2 == 0:
-#                 lst.append(f'+ {random.randint(0, 100)}*x**{i} ')
-#             else:
-#                 lst.append(f'- {random.randint(0, 100)}*x**{i} ')
-#     return ''.join(lst)[1::]
+
+
+'''4 Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+Входные и выходные данные хранятся в отдельных текстовых файлах.
+
+aaaasssdddwwwwwwwwwwwweeeffffff -> 4a3s3d9w3w3e6f
+4a3s3d9w3w3e6f-> aaaasssdddwwwwwwwwwwwweeeffffff'''
+
+# str = 'aaaasssdddwwwwwwwwwwwweeeffffff'
+# lst = []
+# syl = str[0]
+# num = 1
+# for i in range(1, len(str)):
+#    if str[i] == syl:
+#        num += 1
+#        if num == 9:
+#            lst.append(f'{num}{syl}')
+#            num = 0
+#            continue
+#        if i == len(str) - 1:
+#                lst.append(f'{num}{syl}')
+#    else:
+#        lst.append(f'{num}{syl}')
+#        syl = str[i]
+#        num = 1
 #
-#
-# file1 = open('funck1', 'w')
-# file1.write(get_random_funck(7))
-# file1.close()
-#
-# file2 = open('funck2', 'w')
-# file2.write(get_random_funck(5))
-# file2.close()
+# print(*lst, sep='')
 
-'''5 Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
-Коэффициенты могут быть как положительными, так и отрицательными. Степени многочленов могут отличаться.'''
 
-funck1 = '94*x**6 - 26*x**4 + 55*x**2 - 81*x**0'
-funck2 = '1*x**4 - 96*x**2 + 12*x**1 '
 
-def split_funck(str):
-    x = str.split('*x**')
-    str = ' '.join(x)
-    x = str.split('-')
-    str = ' '.join(x)
-    x = str.split('+')
-    str = ' '.join(x)
-    x = str.split(' ')
+'''5* Дан список чисел. Найдите все возрастающие последовательности. Порядок элементов менять нельзя.
 
-    while '' in x:
-        x.remove('')
-    res = list(map(int, x))
+*Пример:*
 
-    make_dict(res)
-    return res
-
-def make_dict(res):
-    dct = {}
-    for i in range(1, len(res), 2):
-        dct[str(res[i])] = res[i - 1]
-    return dct
-
-s1 = split_funck(funck1)
-print(s1)
-d1 = make_dict(s1)
-print(d1)
-
-s2 = split_funck(funck2)
-print(s2)
-d2 = make_dict(s2)
-print(d2)
-
-result_d = {}
-
-print(result_d)
-# d1 = {6: 10, 5: 8, 2: 1}
-# d2 = {6: 1, 5: 2, 2: 8}
-# d3 = {}
-# for i in d1:
-#     d3[i] = d1[i]+d2[i]
-# print(d3)
+[1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3] или [1, 7] или [1, 6, 7] и т.д.'''
